56b92424b653a47c5c2ca8abd81290014368d176
Manel <msoler92@gmail.com>
2024-01-09 11:21:09 +0100

    First commit

---
+++ src/main/java/cat/itacademy/barcelonactiva/solereina/manel/s04/t02/n01/model/services/FruitaService.java
@@ -0,0 +1,48 @@
+package cat.itacademy.barcelonactiva.solereina.manel.s04.t02.n01.model.services;
+
+import cat.itacademy.barcelonactiva.solereina.manel.s04.t02.n01.model.domain.Fruita;
+import cat.itacademy.barcelonactiva.solereina.manel.s04.t02.n01.model.exceptions.NotFoundException;
+import cat.itacademy.barcelonactiva.solereina.manel.s04.t02.n01.model.repository.FruitaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class FruitaService {
+
+    @Autowired
+    FruitaRepository fruitaRepository;
+    public Fruita save(Fruita fruita) {
+        //TODO Review exception handling
+        Fruita returnObject;
+        returnObject = fruitaRepository.save(fruita);
+        return returnObject;
+    }
+    public Fruita findById(int id) throws NotFoundException {
+        Optional<Fruita> fruitaData = fruitaRepository.findById(id);
+
+        if (fruitaData.isEmpty()) {
+            throw new NotFoundException();
+        }
+        return fruitaData.get();
+    }
+
+    public Fruita update(int id, Fruita fruita) throws NotFoundException {
+        Fruita fruitaData = findById(id);
+        fruitaData.setNom(fruita.getNom());
+        fruitaData.setQuantitatQuilos(fruita.getQuantitatQuilos());
+        return save(fruitaData);
+    }
+
+    public List<Fruita> findAll() {
+        return new ArrayList<>(fruitaRepository.findAll());
+    }
+
+    public void deleteById(int id) throws NotFoundException, IllegalArgumentException {
+        findById(id);
+        fruitaRepository.deleteById(id);
+    }
+
+
+}
